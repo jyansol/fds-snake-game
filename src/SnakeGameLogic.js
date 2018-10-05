00e8ba48c1f22d11d7d1c12112087f933e8675d8
jyansol <hahn0406@gmail.com>
2018-10-05 16:43:46 +0900

    snake_game

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -1,39 +1,47 @@
-import {ROWS, COLS} from './config';
+import { ROWS, COLS } from './config';
 
 // NOTE: ROWS, COLS에는 행의 개수, 열의 개수가 저장되어 있습니다.
 // 이 변수를 활용해서 코드를 작성하세요!
 
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
-  this.joints = [
-    {x: 2, y: 0},
-    {x: 1, y: 0},
-    {x: 0, y: 0},
-  ];
+  this.joints = [{ x: 4, y: 0 }, { x: 3, y: 0 }, { x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
 
   // 먹이의 좌표
-  this.fruit = {x: 3, y: 5};
+  this.fruit = { x: 3, y: 5 };
 }
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
+  // 원래 머리를 기준으로 위쪽에 새로운 마디를 붙여준다.
   console.log('up');
-}
+};
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   console.log('down');
-}
+  // this.joints = [{ x: 2, y: 1 }, { x: 2, y: 0 }, { x: 1, y: 0 }];
+  //배열안에 들어있는 객체안의 값
+
+  const tail = this.joints.pop();
+  tail.x = 5;
+  tail.y = this.joints.unshift(tail);
+  console.log(this.joints);
+  // console.log(this.joints[0].y++);
+
+  // console.log(this.joints[1].x++);
+  // console.log(this.joints[2].x++);
+};
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
   console.log('left');
-}
+};
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
   console.log('right');
-}
+};
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
@@ -41,6 +49,6 @@ SnakeGameLogic.prototype.nextState = function() {
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
   return true;
-}
+};
 
 export default SnakeGameLogic;
