882e9329d7717e058f4928ae2ec4670468138fe5
jyansol <hahn0406@gmail.com>
2018-10-06 20:17:54 +0900

    snake_game

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -2,7 +2,6 @@ import { ROWS, COLS } from './config';
 
 // NOTE: ROWS, COLS에는 행의 개수, 열의 개수가 저장되어 있습니다.
 // 이 변수를 활용해서 코드를 작성하세요!
-
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
   this.joints = [{ x: 4, y: 0 }, { x: 3, y: 0 }, { x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 }];
@@ -15,32 +14,55 @@ SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
   // 원래 머리를 기준으로 위쪽에 새로운 마디를 붙여준다.
   console.log('up');
+  const tail = this.joints.pop();
+
+  tail.x = this.joints[0].x;
+  tail.y = this.joints[0].y - 1;
+
+  this.joints.unshift(tail);
+  console.log(this.joints);
 };
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   console.log('down');
-  // this.joints = [{ x: 2, y: 1 }, { x: 2, y: 0 }, { x: 1, y: 0 }];
-  //배열안에 들어있는 객체안의 값
-
   const tail = this.joints.pop();
-  tail.x = 5;
-  tail.y = this.joints.unshift(tail);
-  console.log(this.joints);
-  // console.log(this.joints[0].y++);
 
-  // console.log(this.joints[1].x++);
-  // console.log(this.joints[2].x++);
+  tail.x = this.joints[0].x;
+  tail.y = this.joints[0].y + 1;
+
+  this.joints.unshift(tail);
+  console.log(this.joints);
+  // if (down()) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 };
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
   console.log('left');
+  const tail = this.joints.pop();
+
+  tail.x = this.joints[0].x - 1;
+  tail.y = this.joints[0].y;
+
+  this.joints.unshift(tail);
+
+  console.log(this.joints);
 };
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
   console.log('right');
+  const tail = this.joints.pop();
+
+  tail.x = this.joints[0].x + 1;
+  tail.y = this.joints[0].y;
+
+  this.joints.unshift(tail);
+  console.log(this.joints);
 };
 
 SnakeGameLogic.prototype.nextState = function() {
